24d98453814751d69525b320f103c72d3e55599b
Minmarai <raiminma09@gmail.com>
2026-05-20 17:45:43 +0545

    User DAO

--- src/main/java/com/chargehub/dao/UserDAO.java
+++ src/main/java/com/chargehub/dao/UserDAO.java
@@ -18,7 +18,7 @@ import java.util.*;
  * <p>All database connections are obtained via {@link DBConnection#getConnection()}
  * and are closed automatically using try-with-resources.</p>
  *
- * <p>Author: Minma Rai</p>
+ * <p>Author: Minma Rai IIC</p>
  */
 public class UserDAO {
 
